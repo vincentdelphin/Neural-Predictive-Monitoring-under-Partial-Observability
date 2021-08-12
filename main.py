71793b0841ac0c2e90046243e400c9056d00ecb1
Vincent Delphin <vincentmledelphin@gmail.com>
2021-08-12 15:17:23 +0000

    corrected matrix implementation input.

--- main.py
+++ main.py
@@ -1,4 +1,33 @@
-# Beginning Script
 # Goal: to pass set of states to reach function used in Matlab and return results.
+import matlab.engine
+import numpy as np
 
+# states = [[1, 3], [2, 4]]
+# tFinal needs to be a double
+from numpy import double
 
+tFinal = 200.0
+# String = "reach_values = invokeRC({}, {})".format(states, tFinal)
+
+eng = matlab.engine.start_matlab()
+
+path_of_file = "'C:\\Users\\44772\\AppData\\Roaming\\MathWorks\\MATLAB " \
+               "Add-Ons\\Collections\\CORA\\examples\\hybridDynamics\\hybridAutomaton' "
+eng.cd(
+    r'C:\\Users\\44772\\AppData\\Roaming\\MathWorks\\MATLAB Add-Ons\\Collections\\CORA\\examples\\hybridDynamics\\hybridAutomaton',
+    nargout=0)
+eng.ls(nargout=0)
+
+matrix = [[-900., 25., 0., 0., 0.],
+          [-400., 0., 0., 0., 0.],
+          [0., 0., 0., 0., 0.],
+          [0., 0., 0., 0., 0.]]
+print(matrix)
+print(len(matrix))
+print(len(matrix[0]))
+print('TIME FOR MATLAB')
+# ave = eng.invokeRC6(matrix, 20, nargout=1)
+ave = eng.MatrixInput(matrix, 20, nargout=1)
+
+# print(ave)
+eng.exit()
