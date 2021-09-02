537ba81d162972326f444ed9e0b99995216ea82c
Vincent Delphin <vincentmledelphin@gmail.com>
2021-09-02 02:22:49 +0000

    Added generate function to calculate reachable set for each set of states.

--- main.py
+++ main.py
@@ -15,21 +15,61 @@ eng.cd(
     nargout=0)
 
 A = matlab.double([[-900., 25., 0., 0.],
-                   [-400., 0., 0., 0.],
+                   [-400., 0., 25., 0.],
                    [0., 0., 0., 0.],
                    [0., 0., 0., 0.]])
 
-random_array = np.random.randint(900, size=(4, 4))
+B = matlab.double([[-3., 45., 0., 0.],
+                   [-40., 0., 25., 0.],
+                   [0., 0., 0., 0.],
+                   [0., 0., 0., 0.]])
 
-random_matlab_array = matlab.double(random_array.tolist(), is_complex=False)
+practice_set = [A, B]
 
 
-# Tfinal must be of type double to work
-Tfinal = 20.0
+def generate(practice_set, tFinal):
+    finalset = []
+    i = 0
+    print("size of practice set")
+    print((len(practice_set)))
+    while i < len(practice_set):
+        print(practice_set[i])
+        print("size of this set")
+        print(np.size(practice_set[i]))
+        reach_check = eng.invokeRCNEWEST(practice_set[i], tFinal, nargout=1)
+        finalset.append(reach_check)
+        i = i + 1
+    input("Press any key to terminate the program")
+    # Print bye message
+    print("See you later.")
+    return finalset
+
+
+generate(practice_set, 200.)
+# fucntion([[],[],[]], tfinal):
+#     [], tfinal - pass to reach = get back 1 or 0
+#     append output1 or 0 to output matrix
+#     [], tfinal - pass to reach = get back 1 or 0
+#     append output1 or 0 to output matrix
+#     [], tfinal - pass to reach = get back 1 or 0
+#     append output1 or 0 to output matrix
+# output = [0,1,0]
+
+
+# Initial set radius of [25m, 25m, 0, 0]
+#  around point [-900m, -400m, 0m/s, 0m/s]
+#
+# random_array = np.random.randint(900, size=(4, 4))
+#
+# random_matlab_array = matlab.double(random_array.tolist(), is_complex=False)
+#
+#
+# # Tfinal must be of type double to work
+# Tfinal = 20.0
 
 # Can pass through extra argument for debug mode. InvokeRC6(A,tfinal, debug, nargout=2)
-# ave = eng.invokeRC6(random_matlab_array, tFinal, "debug", nargout=1)
-ave = eng.invokeRC6(random_matlab_array, tFinal, nargout=1)
-print(ave)
+# ave = eng.invokeRC(A, tFinal, "debug", nargout=1)
+# # ave = eng.invokeRC(random_matlab_array, tFinal, nargout=1)
+# print(ave)
 
 eng.exit()
